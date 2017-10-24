020302b63f33f8c2cf5502e8d71539fe0199d952
jpazdera <jpazdera@haverford.edu>
2017-10-24 10:23:37 -0400

    Major edits to reporting

--- report.py
+++ report.py
@@ -27,24 +27,285 @@ def ltpFR3_report(stats):
     :param stats: A dictionary containing the behavioral stats calculated by run_stats.
     """
     stat_plotters = {'prec': plot_prec, 'spc': plot_spc, 'pfr': plot_pfr, 'psr': plot_psr, 'ptr': plot_ptr,
-                      'crp_early': plot_crp_early, 'crp_late': plot_crp_late, 'irt': plot_irt,
+                      'crp_early': plot_crp_early, 'crp_late': plot_crp_late,
                       'pli_early': plot_pli_early, 'pli_late': plot_pli_late, 'eli_early': plot_eli_early,
                       'eli_late': plot_eli_late, 'reps': plot_reps, 'pli_recency': plot_pli_recency}
-
-    stat_order = ['prec', 'spc', 'pfr', 'psr', 'ptr', 'crp_early', 'crp_late', 'irt', 'pli_early', 'pli_late', 'pli_recency', 'eli_early', 'eli_late', 'reps']
 
     for subj in stats:
         pdf = PdfPages('/Users/jessepazdera/Desktop/ltpFR3_reports/' + subj + '.pdf')
         plt.figure(figsize=(40, 30))
         plt.suptitle(subj, fontsize=36)
-        for key in stat_order:
+        for key in stat_plotters:
             if key in stats[subj]:
                 stat_plotters[key](stats[subj][key])
             else:
                 print('ALERT! Missing stat %s for subject %s' % (key, subj))
+        plt.tight_layout(rect=[0, 0.03, 1, 0.95])
         pdf.savefig()
         pdf.close()
         plt.close()
+
+
+def plot_spc(s):
+    plt.subplot(8, 3, 1)
+    plt.plot(range(1, 13), s['f12'])
+    plt.plot(range(1, 25), s['f24'])
+    plt.plot(range(1, 13), s['s12'])
+    plt.plot(range(1, 25), s['s24'])
+    plt.title('SPC')
+    plt.xlabel('Serial Position')
+    plt.ylabel('Probability of Recall')
+    plt.legend(labels=['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.xlim(1, 24)
+    plt.ylim(0, 1)
+    plt.xticks(range(1, 25, 2), range(1, 25, 2))
+
+    plt.subplot(8, 3, 4)
+    plt.plot(range(1, 13), s['v12'])
+    plt.plot(range(1, 25), s['v24'])
+    plt.plot(range(1, 13), s['a12'])
+    plt.plot(range(1, 25), s['a24'])
+    plt.title('SPC')
+    plt.xlabel('Serial Position')
+    plt.ylabel('Probability of Recall')
+    plt.legend(labels=['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.xlim(1, 24)
+    plt.ylim(0, 1)
+    plt.xticks(range(1, 25, 2), range(1, 25, 2))
+
+
+def plot_crp_early(s):
+    plt.subplot(8, 3, 2)
+    plt.plot(range(-3, 4), s['f12'])
+    plt.plot(range(-3, 4), s['f24'])
+    plt.plot(range(-3, 4), s['s12'])
+    plt.plot(range(-3, 4), s['s24'])
+    plt.title('CRP (Early)')
+    plt.xlabel('Lag')
+    plt.ylabel('Cond. Resp. Probability')
+    plt.legend(labels=['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.ylim(0, 1)
+
+    plt.subplot(8, 3, 5)
+    plt.plot(range(-3, 4), s['v12'])
+    plt.plot(range(-3, 4), s['v24'])
+    plt.plot(range(-3, 4), s['a12'])
+    plt.plot(range(-3, 4), s['a24'])
+    plt.title('CRP (Early)')
+    plt.xlabel('Lag')
+    plt.ylabel('Cond. Resp. Probability')
+    plt.legend(labels=['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.ylim(0, 1)
+
+
+def plot_crp_late(s):
+    plt.subplot(8, 3, 3)
+    plt.plot(range(-3, 4), s['f12'])
+    plt.plot(range(-3, 4), s['f24'])
+    plt.plot(range(-3, 4), s['s12'])
+    plt.plot(range(-3, 4), s['s24'])
+    plt.title('CRP (Early)')
+    plt.xlabel('Lag')
+    plt.ylabel('Cond. Resp. Probability')
+    plt.legend(labels=['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.ylim(0, 1)
+
+    plt.subplot(8, 3, 6)
+    plt.plot(range(-3, 4), s['v12'])
+    plt.plot(range(-3, 4), s['v24'])
+    plt.plot(range(-3, 4), s['a12'])
+    plt.plot(range(-3, 4), s['a24'])
+    plt.title('CRP (Late)')
+    plt.xlabel('Lag')
+    plt.ylabel('Cond. Resp. Probability')
+    plt.legend(labels=['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.ylim(0, 1)
+
+
+def plot_pfr(s):
+    plt.subplot(8, 3, 7)
+    plt.plot(range(1, 13), s['f12'])
+    plt.plot(range(1, 25), s['f24'])
+    plt.plot(range(1, 13), s['s12'])
+    plt.plot(range(1, 25), s['s24'])
+    plt.title('PFR')
+    plt.xlabel('Serial Position')
+    plt.ylabel('Probability of First Recall')
+    plt.legend(labels=['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.xlim(1, 24)
+    plt.ylim(0, 1)
+    plt.xticks(range(1, 25, 2), range(1, 25, 2))
+
+    plt.subplot(8, 3, 10)
+    plt.plot(range(1, 13), s['v12'])
+    plt.plot(range(1, 25), s['v24'])
+    plt.plot(range(1, 13), s['a12'])
+    plt.plot(range(1, 25), s['a24'])
+    plt.title('PFR')
+    plt.xlabel('Serial Position')
+    plt.ylabel('Probability of First Recall')
+    plt.legend(labels=['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.xlim(1, 24)
+    plt.ylim(0, 1)
+    plt.xticks(range(1, 25, 2), range(1, 25, 2))
+
+
+def plot_psr(s):
+    plt.subplot(8, 3, 8)
+    plt.plot(range(1, 13), s['f12'])
+    plt.plot(range(1, 25), s['f24'])
+    plt.plot(range(1, 13), s['s12'])
+    plt.plot(range(1, 25), s['s24'])
+    plt.title('PSR')
+    plt.xlabel('Serial Position')
+    plt.ylabel('Probability of Second Recall')
+    plt.legend(labels=['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.xlim(1, 24)
+    plt.ylim(0, 1)
+    plt.xticks(range(1, 25, 2), range(1, 25, 2))
+
+    plt.subplot(8, 3, 11)
+    plt.plot(range(1, 13), s['v12'])
+    plt.plot(range(1, 25), s['v24'])
+    plt.plot(range(1, 13), s['a12'])
+    plt.plot(range(1, 25), s['a24'])
+    plt.title('PSR')
+    plt.xlabel('Serial Position')
+    plt.ylabel('Probability of Second Recall')
+    plt.legend(labels=['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.xlim(1, 24)
+    plt.ylim(0, 1)
+    plt.xticks(range(1, 25, 2), range(1, 25, 2))
+
+
+def plot_ptr(s):
+    plt.subplot(8, 3, 9)
+    plt.plot(range(1, 13), s['f12'])
+    plt.plot(range(1, 25), s['f24'])
+    plt.plot(range(1, 13), s['s12'])
+    plt.plot(range(1, 25), s['s24'])
+    plt.title('PTR')
+    plt.xlabel('Serial Position')
+    plt.ylabel('Probability of Third Recall')
+    plt.legend(labels=['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.xlim(1, 24)
+    plt.ylim(0, 1)
+    plt.xticks(range(1, 25, 2), range(1, 25, 2))
+
+    plt.subplot(8, 3, 12)
+    plt.plot(range(1, 13), s['v12'])
+    plt.plot(range(1, 25), s['v24'])
+    plt.plot(range(1, 13), s['a12'])
+    plt.plot(range(1, 25), s['a24'])
+    plt.title('PTR')
+    plt.xlabel('Serial Position')
+    plt.ylabel('Probability of Third Recall')
+    plt.legend(labels=['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.xlim(1, 24)
+    plt.ylim(0, 1)
+    plt.xticks(range(1, 25, 2), range(1, 25, 2))
+
+
+def plot_pli_recency(s):
+    plt.subplot(8, 3, 13)
+    plt.plot(range(1, 7), s['f12'])
+    plt.plot(range(1, 7), s['f24'])
+    plt.plot(range(1, 7), s['s12'])
+    plt.plot(range(1, 7), s['s24'])
+    plt.title('PLI Recency')
+    plt.xlabel('Number of Lists Back')
+    plt.ylabel('Ratio of PLIs')
+    plt.legend(labels=['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+
+    plt.subplot(8, 3, 16)
+    plt.plot(range(1, 7), s['v12'])
+    plt.plot(range(1, 7), s['v24'])
+    plt.plot(range(1, 7), s['a12'])
+    plt.plot(range(1, 7), s['a24'])
+    plt.title('PLI Recency')
+    plt.xlabel('Number of Lists Back')
+    plt.ylabel('Ratio of PLIs')
+    plt.legend(labels=['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+
+
+def plot_pli_early(s):
+    plt.subplot(8, 3, 14)
+    plt.bar([1, 2, 3, 4], [s['f12'], s['f24'], s['s12'], s['s24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.title('PLI (Early)')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. PLIs per List')
+
+    plt.subplot(8, 3, 17)
+    plt.bar([1, 2, 3, 4], [s['v12'], s['v24'], s['a12'], s['a24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.title('PLI (Early)')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. PLIs per List')
+
+
+def plot_pli_late(s):
+    plt.subplot(8, 3, 15)
+    plt.bar([1, 2, 3, 4], [s['f12'], s['f24'], s['s12'], s['s24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.title('PLI (Late)')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. PLIs per List')
+
+    plt.subplot(8, 3, 18)
+    plt.bar([1, 2, 3, 4], [s['v12'], s['v24'], s['a12'], s['a24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.title('PLI (Late)')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. PLIs per List')
+
+
+def plot_eli_early(s):
+    plt.subplot(8, 3, 19)
+    plt.bar([1, 2, 3, 4], [s['f12'], s['f24'], s['s12'], s['s24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.title('ELI (Early)')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. ELIs per List')
+
+    plt.subplot(8, 3, 22)
+    plt.bar([1, 2, 3, 4], [s['v12'], s['v24'], s['a12'], s['a24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.title('ELI (Early)')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. ELIs per List')
+
+
+def plot_eli_late(s):
+    plt.subplot(8, 3, 20)
+    plt.bar([1, 2, 3, 4], [s['f12'], s['f24'], s['s12'], s['s24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.title('ELI (Late)')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. ELIs per List')
+
+    plt.subplot(8, 3, 23)
+    plt.bar([1, 2, 3, 4], [s['v12'], s['v24'], s['a12'], s['a24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.title('ELI (Late)')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. ELIs per List')
+
+
+def plot_reps(s):
+    plt.subplot(8, 3, 21)
+    plt.bar([1, 2, 3, 4], [s['f12'], s['f24'], s['s12'], s['s24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Fast/12', 'Fast/24', 'Slow/12', 'Slow/24'])
+    plt.title('Repetitions')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. Repetitions per List')
+
+    plt.subplot(8, 3, 24)
+    plt.bar([1, 2, 3, 4], [s['v12'], s['v24'], s['a12'], s['a24']], align='center', color='#D5D5D5')
+    plt.xticks([1, 2, 3, 4], ['Visual/12', 'Visual/24', 'Auditory/12', 'Auditory/24'])
+    plt.title('Repetitions')
+    plt.xlabel('List Length')
+    plt.ylabel('Avg. Repetitions per List')
 
 
 def plot_prec(s):
@@ -59,134 +320,5 @@ def plot_prec(s):
     '''
 
 
-def plot_spc(s):
-    plt.subplot(4, 3, 1)
-    plt.plot(range(1, 13), s[12], '-ko')
-    plt.plot(range(1, 25), s[24], '--kD')
-    plt.title('SPC')
-    plt.xlabel('Serial Position')
-    plt.ylabel('Probability of Recall')
-    plt.legend(labels=['Length 12', 'Length 24'])
-    plt.xlim(1, 24)
-    plt.ylim(0, 1)
-    plt.xticks(range(1, 25, 2), range(1, 25, 2))
-
-
-def plot_pfr(s):
-    plt.subplot(4, 3, 4)
-    plt.plot(range(1, 13), s[12], '-ko')
-    plt.plot(range(1, 25), s[24], '--kD')
-    plt.title('PFR')
-    plt.xlabel('Serial Position')
-    plt.ylabel('Probability of First Recall')
-    plt.legend(labels=['Length 12', 'Length 24'])
-    plt.xlim(1, 24)
-    plt.ylim(0, 1)
-    plt.xticks(range(1, 25, 2), range(1, 25, 2))
-
-
-def plot_psr(s):
-    plt.subplot(4, 3, 5)
-    plt.plot(range(1, 13), s[12], '-ko')
-    plt.plot(range(1, 25), s[24], '--kD')
-    plt.title('PSR')
-    plt.xlabel('Serial Position')
-    plt.ylabel('Probability of Second Recall')
-    plt.legend(labels=['Length 12', 'Length 24'])
-    plt.xlim(1, 24)
-    plt.ylim(0, 1)
-    plt.xticks(range(1, 25, 2), range(1, 25, 2))
-
-
-def plot_ptr(s):
-    plt.subplot(4, 3, 6)
-    plt.plot(range(1, 13), s[12], '-ko')
-    plt.plot(range(1, 25), s[24], '--kD')
-    plt.title('PTR')
-    plt.xlabel('Serial Position')
-    plt.ylabel('Probability of Third Recall')
-    plt.legend(labels=['Length 12', 'Length 24'])
-    plt.xlim(1, 24)
-    plt.ylim(0, 1)
-    plt.xticks(range(1, 25, 2), range(1, 25, 2))
-
-
-def plot_crp_early(s):
-    plt.subplot(4, 3, 2)
-    plt.plot(range(-3, 4), s[12], '-ko')
-    plt.plot(range(-3, 4), s[24], '--kD')
-    plt.title('CRP (Early)')
-    plt.xlabel('Lag')
-    plt.ylabel('Cond. Resp. Probability')
-    plt.legend(labels=['Length 12', 'Length 24'])
-    plt.ylim(0, 1)
-
-
-def plot_crp_late(s):
-    plt.subplot(4, 3, 3)
-    plt.plot(range(-3, 4), s[12], '-ko')
-    plt.plot(range(-3, 4), s[24], '--kD')
-    plt.title('CRP (Late)')
-    plt.xlabel('Lag')
-    plt.ylabel('Cond. Resp. Probability')
-    plt.legend(labels=['Length 12', 'Length 24'])
-    plt.ylim(0, 1)
-
-
 def plot_irt(s):
     pass
-
-
-def plot_pli_early(s):
-    plt.subplot(4, 3, 7)
-    plt.bar([1, 2], [s[12], s[24]], align='center', color='#D5D5D5')
-    plt.xticks([1, 2], ['12', '24'])
-    plt.title('PLI (Early)')
-    plt.xlabel('List Length')
-    plt.ylabel('Avg. PLIs per List')
-
-
-def plot_pli_late(s):
-    plt.subplot(4, 3, 8)
-    plt.bar([1, 2], [s[12], s[24]], align='center', color='#D5D5D5')
-    plt.xticks([1, 2], ['12', '24'])
-    plt.title('PLI (Late)')
-    plt.xlabel('List Length')
-    plt.ylabel('Avg. PLIs per List')
-
-
-def plot_eli_early(s):
-    plt.subplot(4, 3, 10)
-    plt.bar([1, 2], [s[12], s[24]], align='center', color='#D5D5D5')
-    plt.xticks([1, 2], ['12', '24'])
-    plt.title('ELI (Early)')
-    plt.xlabel('List Length')
-    plt.ylabel('Avg. ELIs per List')
-
-
-def plot_eli_late(s):
-    plt.subplot(4, 3, 11)
-    plt.bar([1, 2], [s[12], s[24]], align='center', color='#D5D5D5')
-    plt.xticks([1, 2], ['12', '24'])
-    plt.title('ELI (Late)')
-    plt.xlabel('List Length')
-    plt.ylabel('Avg. ELIs per List')
-
-
-def plot_reps(s):
-    plt.subplot(4, 3, 12)
-    plt.bar([1, 2], [s[12], s[24]], align='center', color='#D5D5D5')
-    plt.xticks([1, 2], ['12', '24'])
-    plt.title('Reps')
-    plt.xlabel('List Length')
-    plt.ylabel('Avg. Repetitions per List')
-
-
-def plot_pli_recency(s):
-    plt.subplot(4, 3, 9)
-    plt.plot(range(1, 7), s[12], '-ko')
-    plt.plot(range(1, 7), s[24], '--kD')
-    plt.title('PLI Recency')
-    plt.xlabel('Number of Lists Back')
-    plt.ylabel('Ratio of PLIs')
-    plt.legend(labels=['Length 12', 'Length 24'])
