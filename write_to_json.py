2819cdf53e2b7fc949de15f04dbf41f337def9b1
jpazdera <jpazdera@haverford.edu>
2017-10-24 11:09:19 -0400

    Fixed missing index

--- write_to_json.py
+++ write_to_json.py
@@ -5,7 +5,7 @@ import numpy as np
 def write_to_json(data, stats, behmat_json, stat_json):
     # Convert numpy arrays to lists
     for subj in data:
-        for field in data:
+        for field in data[subj]:
             if isinstance(data[subj][field], np.ndarray):
                 data[subj][field] = data[subj][field].tolist()
 
