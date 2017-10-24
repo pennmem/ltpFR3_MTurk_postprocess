cd248ceb34869e4a7d6d8e7362291004e7cd6d6d
jpazdera <jpazdera@haverford.edu>
2017-10-24 11:07:04 -0400

    Added saving of behavioral data matrices and statistics to JSON files

--- process_ltpFR3_MTurk.py
+++ process_ltpFR3_MTurk.py
@@ -1,7 +1,7 @@
-import json
 import psiturk_tools
 from run_stats import run_stats
 from report import ltpFR3_report
+from write_to_json import write_to_json
 
 # Set paths
 db_url = 'sqlite:////data/eeg/scalp/ltp/ltpFR3_MTurk/ltpFR3_anonymized.db'  # url for the database in which raw psiturk ouput is stored
@@ -14,13 +14,11 @@ stat_json = '/data/eeg/scalp/ltp/ltpFR3_MTurk/ltpFR3_stats.json'
 # Load the data from the psiTurk experiment database and process it into a dictionary, then save to JSON
 data = psiturk_tools.load_psiturk_data(db_url, table_name)
 data = psiturk_tools.process_psiturk_data(data, dict_path)
-with open(behmat_json, 'w') as f:
-    json.dump(data, f)
 
 # Run stats on the data and save to JSON
 stats = run_stats(data)
-with open(stat_json, 'w') as f:
-    json.dump(stats, f)
 
 # Generate a PDF report for each participant, along with an aggregate report
 ltpFR3_report(stats)
+
+write_to_json(data, stats, behmat_json, stat_json)
---
+++ write_to_json.py
@@ -0,0 +1,22 @@
+import json
+import numpy as np
+
+
+def write_to_json(data, stats, behmat_json, stat_json):
+    # Convert numpy arrays to lists
+    for subj in data:
+        for field in data:
+            if isinstance(data[subj][field], np.ndarray):
+                data[subj][field] = data[subj][field].tolist()
+
+    with open(behmat_json, 'w') as f:
+        json.dump(data, f)
+
+    for subj in stats:
+        for stat in stats[subj]:
+            for f in stats[subj][stat]:
+                if isinstance(stats[subj][stat][f], np.ndarray):
+                    stats[subj][stat][f] = stats[subj][stat][f].tolist()
+
+    with open(stat_json, 'w') as f:
+        json.dump(stats, f)
