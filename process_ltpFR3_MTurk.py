8a197737cf58039cb22911f2593b22ce31fef6f6
jpazdera <jpazdera@haverford.edu>
2017-10-24 10:22:50 -0400

    Removed unused variable

--- process_ltpFR3_MTurk.py
+++ process_ltpFR3_MTurk.py
@@ -3,11 +3,10 @@ from run_stats import run_stats
 from report import ltpFR3_report
 
 # Set paths
-db_url = 'sqlite:////Users/jessepazdera/AtomProjects/ltpFR3-MTurk/participants.db'  # url for the database in which raw psiturk ouput is stored
+db_url = 'sqlite:////Users/jessepazdera/Desktop/participants_anonymized.db'  # url for the database in which raw psiturk ouput is stored
 table_name = 'ltpFR3'  # table of the database
 dict_path = 'webster_dictionary.txt'  # dictionary to use when looking for ELIs and correcting spelling
 save_file = 'ltpFR3.data'
-exp_data_dir = ''  # path to data dir on circ2
 
 # Load the data from the psiTurk experiment database and process it into a dictionary
 data = psiturk_tools.load_psiturk_data(db_url, table_name)
