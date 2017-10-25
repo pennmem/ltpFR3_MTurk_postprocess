46c0e50849ca762c23c26839e7f83a526b73b762
jpazdera <jpazdera@haverford.edu>
2017-10-25 11:37:30 -0400

    Added support for rec_per_trial and math_per_trial

--- write_to_json.py
+++ write_to_json.py
@@ -21,9 +21,12 @@ def write_to_json(data, stats, behmat_json, stat_json):
 
     for subj in stats:
         for stat in stats[subj]:
-            for f in stats[subj][stat]:
-                if isinstance(stats[subj][stat][f], np.ndarray):
-                    stats[subj][stat][f] = stats[subj][stat][f].tolist()
+            if stat in ('rec_per_trial', 'math_per_trial'):
+                stats[subj][stat] = stats[subj][stat].tolist()
+            else:
+                for f in stats[subj][stat]:
+                    if isinstance(stats[subj][stat][f], np.ndarray):
+                        stats[subj][stat][f] = stats[subj][stat][f].tolist()
 
     with open(stat_json, 'w') as f:
         json.dump(stats, f)
