19ba75d76af0a5d23f551f2de5d88ebf736595cc
jpazdera <jpazdera@haverford.edu>
2018-03-01 11:44:20 -0500

    Bug fix to special exclusion IRT (was using excluded trials instead of non-excluded ones)

--- run_stats.py
+++ run_stats.py
@@ -139,7 +139,7 @@ def stats_for_subj(sub, condi, recalls, wasrec, ffr_wasrec, rt, recw, presw, int
             stats['temp_fact'][f] = temp_fact(frecalls, fsub, ll)[0]
             stats['irt'][f] = irt_subj(frt, frecalls, ll)
             # Special version of the IRT which excludes trials that had a recall within the last 10 seconds
-            rt_exclusion_mask = np.max(frt, axis=1) > 50000
+            rt_exclusion_mask = np.max(frt, axis=1) <= 50000
             if rt_exclusion_mask.sum() > 0:
                 stats['irt_sp_excl'][f] = irt_subj(frt[rt_exclusion_mask], frecalls[rt_exclusion_mask], ll)
             else:
